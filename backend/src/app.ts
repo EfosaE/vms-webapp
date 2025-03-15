ee19851a51fe596e691e22bca2b5711c9b93bc3d
EfosaE <osamwonyiefosa02@gmail.com>
2025-03-15 15:18:34 +0100

    first auth abd db

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import authRouter from "./routes/auth.route";
+import AppError from "./utils/appError";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,6 +8,11 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello from Express + TypeScript!");
 });
+app.use('/api/v1/auth/login', authRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`cant find api path ${req.originalUrl}`, 404))
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ backend/src/routes/auth.route.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+
+const authRouter = Router();
+
+// authRouter.route('/signup').post(signUp);
+
+export default authRouter;
